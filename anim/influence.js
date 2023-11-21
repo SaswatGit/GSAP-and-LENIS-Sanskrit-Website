fc1af9915efe2a8981e3c2bc9a74873736afae61
Saswat <gitsaswat@gmail.com>
2023-11-21 20:35:41 +0530

    Fixing responsiveness

--- anim/influence.js
+++ anim/influence.js
@@ -7,17 +7,17 @@ gsap.to("#influence .head h1:nth-child(1)", {y: "700%", scrollTrigger: {
 }});
 gsap.to("#influence .head h1:nth-child(2)", {y: "-700%", scrollTrigger: {
     trigger: "#influence",
-    start: "top 100%",
+    start: "-50% 100%",
     scrub: 0.5,
 }});
 gsap.from("#influence .para", {x: "50%", duration: 0.5, stagger: 0.5, scrollTrigger: {
     trigger: "#influence",
-    start: "top 100%",
+    start: "-50% 100%",
     scrub: 0.2,
 }});
 gsap.from("#influence .points", {x: "-40%", duration: 0.5, stagger: 0.5, scrollTrigger: {
     trigger: "#influence",
-    start: "top bottom",
+    start: "-50% bottom",
     scrub: 0.2,
 }});
 
--- anim/writing.js
+++ anim/writing.js
@@ -2,17 +2,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.from("#writing .head h1", {y: "-100%", duration: 0.5, scrollTrigger: {
     trigger: "#writing .head h1",
-    start: "top bottom",
+    start: "-50% bottom",
     scrub: 0.5,
 }});
 gsap.from("#writing .para", {x: "30%", duration: 0.5, stagger: 0.5, scrollTrigger: {
     trigger: "#writing",
-    start: "top center",
+    start: "-50% center",
     scrub: 0.2,
 }});
 gsap.from("#writing .points", {x: "-20%", duration: 0.5, stagger: 0.5, scrollTrigger: {
     trigger: "#writing",
-    start: "top center",
+    start: "-50% center",
     scrub: 0.2,
 }});
 
